1fefa1f9cfedd895eeaf6133a274865bd5ac09c4
Ural Shrestha <shresthaural100@gmail.com>
2021-12-14 09:50:10 +0545

    changed python interpreter

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -519,8 +519,8 @@
   ;; :hook (python-mode . lsp-deferred)
   :custom
   ;; NOTE: Set these if Python 3 is called "python3" on your system!
-  ;; (python-shell-interpreter "python3")
-  ;; (dap-python-executable "python3")
+  (python-shell-interpreter "python3")
+  (dap-python-executable "python3")
   (dap-python-debugger 'debugpy)
   :config
   (require 'dap-python))
