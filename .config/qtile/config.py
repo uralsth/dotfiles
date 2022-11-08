2c1d54b018450eb0734f6032320b23a529945732
uralsth <shresthaural100@gmail.com>
2022-11-08 16:17:22 +0545

    configure bar and added new function and bindgings

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -4,17 +4,20 @@ from libqtile.config import Click, Drag, DropDown, Group, Key, KeyChord, Match, 
 from libqtile.lazy import lazy
 import os
 import subprocess
-from libqtile import hook
+from libqtile import hook, qtile
 
 
 mod = "mod4"
 terminal = "st"
+
+# Custom Functions
 
 # Minimize and Maximize gaps 
 @lazy.layout.function
 def change_layout_gap(layout, adjustment):
     layout.margin += adjustment
     layout.cmd_reset()
+
 
 # Resize floating window
 @lazy.window.function 
@@ -28,10 +31,92 @@ def move_floating_window(window, x: int = 0, y: int = 0):
     new_y = window.float_y + y
     window.cmd_set_position_floating(new_x, new_y)
 
-# keys = [
-# ]
+
+# sticky window feature              
+win_list = []
+@lazy.function
+def stick_win(qtile):
+    global win_list
+    win_list.append(qtile.current_window)
+
+@lazy.function
+def unstick_win(qtile):
+    global win_list
+    if qtile.current_window in win_list:
+        win_list.remove(qtile.current_window)
+
+
+@hook.subscribe.setgroup
+def win_move():
+    for sw in win_list:
+        sw.togroup(qtile.current_group.name)
+
+
+@hook.subscribe.client_killed
+def remove_sticky_windows(window):
+    if window in win_list:
+        win_list.remove(window)
+
+
+# Make Firefox PIP sticky by default
+@hook.subscribe.client_managed
+def auto_sticky_windows(window):
+    info = window.info()
+    if (info['wm_class'] == ['Toolkit', 'firefox']
+            and info['name'] == 'Picture-in-Picture'):
+        win_list.append(window)
+
+
+# Hide bar at startup
+@hook.subscribe.startup
+def somestartup():
+    qtile.cmd_hide_show_bar('all')
+
+
+# autostart at startup
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~')
+    subprocess.Popen([home + '/.config/qtile/autostart.sh'])
+
+
+# Work around for matching Spotify
+@hook.subscribe.client_new
+def slight_delay(window):
+    time.sleep(0.04)
+
+
+# If Spotify opens move it to group 6
+@hook.subscribe.client_name_updated
+def spotify(window):
+    if window.name == 'Spotify':
+        window.togroup(group_name='5')
+
+# If mpv opens, change floating to tile
+# commented one: float it at pos x, y, w, h, borderwidth, border color
+# @hook.subscribe.client_new
+# def disable_floating(window):
+#     rules = [
+#         Match(wm_class="mpv")
+#     ]
+
+#     if any(window.match(rule) for rule in rules):
+#         window.togroup(qtile.current_group.name)
+#         window.cmd_disable_floating()
+
+
+
+# Scratchpad fix
+@hook.subscribe.group_window_add
+def switchtogroup(group, window):
+    if 'term' not in window.get_wm_class() | 'pulsemixer' not in window.get_wm_class() | 'ncmpcpp' not in window.get_wm_class():  
+        group.cmd_toscreen()
+
+
+
+## KEYBINDINGS
+
 keys = [
-    
 
     # Movements Bindings
     Key([mod], "h",
@@ -121,6 +206,9 @@ keys = [
         desc='increase gap by 1'),
 
 
+    Key([mod], 'b',
+        lazy.hide_show_bar(),
+        desc='Toggle bar visibility'),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
@@ -141,22 +229,25 @@ keys = [
     Key([mod], "Escape", lazy.screen.toggle_group(), desc="Move to the last visited group"),
 
 
-    
-
-
+    # Resizing Floating Window
     KeyChord([mod],"x", [
         Key([], "h", resize_floating_window(width=10), desc='increase width by 10'), 
         Key([], "l", resize_floating_window(width=-10), desc='decrease width by 10'), 
         Key([], "k", resize_floating_window(height=10), desc='increase height by 10'), 
         Key([], "j", resize_floating_window(height=-10), desc='decrease height by 10'),
         ],
-                mode=True,
-                name="Resize Floating Windows"),
+             mode=True,
+             name="Resize Floating Windows"),
+
+    # Sticky Window Bindings
+    Key([mod], "w", stick_win, desc="stick win"),
+    Key([mod, "shift"], "w", unstick_win, desc="unstick win"),
 ]
 
 
 
-# Create labels for groups and assign them a default layout.
+## Groups
+
 groups = []
 
 group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -237,6 +328,11 @@ keys.append(
             Key([], "j", lazy.group["scratchpad"].dropdown_toggle("pulsemixer")),
             Key([], "k", lazy.group["scratchpad"].dropdown_toggle("ncmpcpp")),
              ]),)
+
+
+
+## Layouts
+
 layouts = [
     # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     # layout.Max(),
@@ -244,19 +340,22 @@ layouts = [
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    layout.MonadTall(border_width=2, margin=20),
+    layout.MonadTall(border_width=2, margin=20, border_focus="#6da3bb"),
     # layout.MonadWide(),
     # layout.RatioTile(),
     # layout.Tile(),
     # layout.TreeTab(),
     # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Floating(),
 ]
+
+
+## Widgets
 
 widget_defaults = dict(
     font="Roboto, NotoSans Nerd Font",
     fontsize=13,
-    padding=3,
+    padding=2,
 )
 extension_defaults = widget_defaults.copy()
 
@@ -264,25 +363,73 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.CurrentLayout(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                # widget.TextBox("default config", name="default"),
-                # widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                # widget.QuickExit(),
+             widget.Spacer(
+                 length=10,
+                 background='#000000'
+             ),
+             widget.GroupBox(),
+             widget.Spacer(
+                 length=10,
+                 background='#000000'
+             ),
+             widget.TaskList(
+                 icon_size=18,
+                 max_title_width=200,
+                 txt_floating=" ",
+                 txt_maximized="类 ",
+                 txt_minimized="絛 ",
+             ),
+             widget.Chord(
+                 chords_colors={
+                     "launch": ("#ff0000", "#ffffff"),
+             },
+                 name_transform=lambda name: name.upper(),
+            ),
+             # widget.Mpris2(
+             #     name = "Spotify",
+             #     objname = "org.mpris.MediaPlayer2.spotify",
+             #     display_metadata = ['xesam:title', 'xesam:artist'],
+             #     font = "Fira Code Nerd Font",
+             #     width = 150,
+             # ),
+
+             widget.Systray(),
+             widget.Spacer(
+                 length=10,
+                 background='#000000'
+             ),
+             widget.Backlight(
+                 backlight_name="intel_backlight",
+                 scroll=True,
+                 step=5,
+             ),
+             widget.Battery(
+                battery=0,
+                 charge_char="",
+                 discharge_char="",
+                 empty_char="",
+                 full_char="",
+                 unknown_char="",
+                 format='{char} ',
+                 show_short_text=False,
+                 update_interval=15,
+             ),
+             widget.Clock(
+                format = "%A, %B %d %H:%M:%S",
+                 # format="%-H:%M:%S"
+             ),
+             # widget.QuickExit(),
+
+             widget.CurrentLayoutIcon(
+                 custom_icons_paths=["~/.config/qtile/assets/icons/"],
+                 scale=0.7,
+             ),
+             widget.Spacer(
+                 length=6,
+                 background='#000000'
+             ),
             ],
-            24,
+            25,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
@@ -332,35 +479,4 @@ wl_input_rules = None
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D"
-
-
-# autostart at startup
-@hook.subscribe.startup_once
-def autostart():
-    home = os.path.expanduser('~')
-    subprocess.Popen([home + '/.config/qtile/autostart.sh'])
-
-# Work around for matching Spotify
-@hook.subscribe.client_new
-def slight_delay(window):
-    time.sleep(0.04)
-
-# If Spotify opens move it to group 6
-@hook.subscribe.client_name_updated
-def spotify(window):
-    if window.name == 'Spotify':
-        window.togroup(group_name='5')
-
-# If mpv opens, change floating to tile
-# commented one: float it at pos x, y, w, h, borderwidth, border color
-@hook.subscribe.client_managed
-def repos(window):
-    if window.get_wm_class() and 'mpv' in window.get_wm_class():
-        window.floating = False
-        # window.place(1200, 650, 640, 360, 2, "#ffffff")
-
-@hook.subscribe.group_window_add
-def switchtogroup(group, window):
-    if 'term' not in window.get_wm_class() | 'pulsemixer' not in window.get_wm_class() | 'ncmpcpp' not in window.get_wm_class():  
-        group.cmd_toscreen()
+wmname = "qtile"
